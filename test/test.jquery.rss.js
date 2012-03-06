43a94b52f2687b39173c4f01c8a630714576bc61
sdepold <sascha@depold.com>
2012-03-06 15:32:54 +0100

    tests for limit, layoutTemplate and entryTemplate

--- test/test.jquery.rss.js
+++ test/test.jquery.rss.js
@@ -13,4 +13,35 @@ describe('jquery.rss', function() {
       done()
     })
   })
+
+  it("renders 2 list entries if limit is set to 2", function(done) {
+    $container.rss('http://feeds.feedburner.com/dawanda', {
+      limit: 2
+    }, function() {
+      expect(jQuery('li', $container)).to.have.length(2)
+      done()
+    })
+  })
+
+  it("renders the defined entry template", function(done) {
+    $container.rss('http://feeds.feedburner.com/dawanda', {
+      limit: 1,
+      entryTemplate: '<li>foo</li>'
+    }, function() {
+      var renderedContent = $container.html().split('\n').map(function(s){ return s.trim() }).join('').trim()
+      expect(renderedContent).to.match(/<ul><li>foo<\/li><\/ul>/)
+      done()
+    })
+  }),
+
+  it("renders the defined layout template", function(done) {
+    $container.rss('http://feeds.feedburner.com/dawanda', {
+      limit: 1,
+      layoutTemplate: 'foo<ul>{entries}</ul>bar'
+    }, function() {
+      var renderedContent = $container.html().replace(/\n/g, '')
+      expect(renderedContent).to.match(/foo<ul>.*<\/ul>/)
+      done()
+    })
+  })
 })
