306e83f9391e7e933247451680915c4d5820d887
sdepold <sascha@depold.com>
2012-03-07 09:53:19 +0100

    use phantomjs directly instead of phantomjs-node

--- test/index.js
+++ test/index.js
@@ -1,49 +1,46 @@
-var testFile = encodeURI('file://' + __dirname + '/index.html')
+var page = new WebPage()
+  , url  = encodeURI('file://' + require('fs').absolute('test/index.html'))
 
-require("phantom").create(function(phantom) {
-  phantom.createPage(function(page) {
-    page.open(testFile, function(status) {
-      if (status !== "success") {
-        console.log("Unable to access network");
-        phantom.exit();
-      } else {
-        checkMochaResults(phantom, page)
-      }
-    })
-  })
+page.open(url, function(status) {
+  if (status !== "success") {
+    console.log("Unable to access network");
+    phantom.exit(1);
+  } else {
+    checkMochaResults(page)
+  }
 })
 
-var waitUntilTestsAreFinished = function(page, callback) {
+var waitUntilTestsAreFinished = function(callback) {
   var prevFinishedCount = 0
 
   var waitIntervalId = setInterval(function() {
-    page.evaluate(function() {
+    var currentFinishedCount = page.evaluate(function() {
       return document.getElementsByClassName("test").length
-    }, function(currentFinishedCount) {
-      if(prevFinishedCount == currentFinishedCount) {
-        // count has not changed => tests are most likely done
-        clearInterval(waitIntervalId)
-        callback && callback()
-      } else {
-        // count has changed => save the current count and wait for next interval
-        prevFinishedCount = currentFinishedCount
-      }
     })
+
+    if(prevFinishedCount == currentFinishedCount) {
+      // count has not changed => tests are most likely done
+      clearInterval(waitIntervalId)
+      callback && callback()
+    } else {
+      // count has changed => save the current count and wait for next interval
+      prevFinishedCount = currentFinishedCount
+    }
   }, 500)
 }
 
 
-var checkMochaResults = function(phantom, page) {
-  waitUntilTestsAreFinished(page, function() {
-    page.evaluate(function() {
+var checkMochaResults = function(page) {
+  waitUntilTestsAreFinished(function() {
+    var failCount = page.evaluate(function() {
       return document.getElementsByClassName("test fail").length
-    }, function(failCount) {
-      var exitCode = ((failCount == 0) ? 0 : 1)
-
-      console.log('Failed specs: ' + failCount)
-      console.log('Exit code: ' + exitCode)
-
-      phantom.exit(exitCode)
     })
+
+    var exitCode = ((failCount == 0) ? 0 : 1)
+
+    console.log('Failed specs: ' + failCount)
+    console.log('Exit code: ' + exitCode)
+
+    phantom.exit(exitCode)
   })
 }
