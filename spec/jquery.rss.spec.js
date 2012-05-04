e9242905390bafc2721339548c28657080a23ee9
sdepold <sascha@depold.com>
2012-05-04 09:24:42 +0200

    converted mocha tests into buster tests

--- spec/jquery.rss.spec.js
+++ spec/jquery.rss.spec.js
@@ -15,4 +15,60 @@ describe('jquery.rss', function() {
       done()
     })
   })
+
+  it("renders 2 list entries if limit is set to 2", function(done) {
+    var $container = this.element
+
+    $container.rss('http://feeds.feedburner.com/dawanda', {
+      limit: 2
+    }, function() {
+      expect(jQuery('li', $container).length).toEqual(2)
+      done()
+    })
+  })
+
+  it("renders the defined entry template", function(done) {
+    var $container = this.element
+
+    $container.rss('http://feeds.feedburner.com/dawanda', {
+      limit: 1,
+      entryTemplate: '<li>foo</li>'
+    }, function() {
+      var renderedContent = $container.html().split('\n').map(function(s){ return s.trim() }).join('').trim()
+      expect(renderedContent).toMatch(/<ul><li>foo<\/li><\/ul>/)
+      done()
+    })
+  })
+
+  it("renders the defined layout template", function(done) {
+    var $container = this.element
+
+    $container.rss('http://feeds.feedburner.com/dawanda', {
+      limit: 1,
+      layoutTemplate: 'foo<ul>{entries}</ul>bar'
+    }, function() {
+      var renderedContent = $container.html().replace(/\n/g, '')
+      expect(renderedContent).toMatch(/foo<ul>.*<\/ul>/)
+      done()
+    })
+  })
+
+  it("supports custom tokens", function(done) {
+    var $container = this.element
+
+    $container.rss('http://feeds.feedburner.com/dawanda', {
+      limit: 1,
+      entryTemplate: '<li>{myCustomStaticToken} {myCustomDynamicToken}</li>',
+      tokens: {
+        myCustomStaticToken: 'static',
+        myCustomDynamicToken: function() {
+          return 'dynamic'
+        }
+      }
+    }, function() {
+      var renderedContent = $container.html().split('\n').map(function(s){ return s.trim() }).join('').trim()
+      expect(renderedContent).toMatch(new RegExp("<ul><li>static dynamic</li></ul>"))
+      done()
+    })
+  })
 })
