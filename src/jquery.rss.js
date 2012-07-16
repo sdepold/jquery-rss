798b2e8ff88b68eee26890db3b20fb89ac0afffa
sdepold <sascha@depold.com>
2012-07-16 09:54:03 +0200

    refactored logic to be able to change the appearance of the entries

--- src/jquery.rss.js
+++ src/jquery.rss.js
@@ -2,9 +2,12 @@
   "use strict";
 
   var RSS = function(target, url, options, callback) {
-    this.target = target
-    this.url = url
-    this.html = []
+    this.target       = target
+    this.url          = url
+    this.html         = []
+    this.effectQueue  = []
+    this.callback     = callback
+
     this.options = jQuery.extend({
       ssl: false,
       limit: null,
@@ -12,9 +15,9 @@
       layoutTemplate: '<ul>{entries}</ul>',
       entryTemplate: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>',
       tokens: {},
-      outputMode: 'json'
+      outputMode: 'json',
+      effect: 'show'
     }, options || {})
-    this.callback = callback
   }
 
   RSS.htmlTags = ["doctype", "html", "head", "title", "base", "link", "meta", "style", "script", "noscript", "body", "article", "nav", "aside", "section", "header", "footer", "h1-h6", "hgroup", "address", "p", "hr", "pre", "blockquote", "ol", "ul", "li", "dl", "dt", "dd", "figure", "figcaption", "div", "table", "caption", "thead", "tbody", "tfoot", "tr", "th", "td", "col", "colgroup", "form", "fieldset", "legend", "label", "input", "button", "select", "datalist", "optgroup", "option", "textarea", "keygen", "output", "progress", "meter", "details", "summary", "command", "menu", "del", "ins", "img", "iframe", "embed", "object", "param", "video", "audio", "source", "canvas", "track", "map", "area", "a", "em", "strong", "i", "b", "u", "s", "small", "abbr", "q", "cite", "dfn", "sub", "sup", "time", "code", "kbd", "samp", "var", "mark", "bdi", "bdo", "ruby", "rt", "rp", "span", "br", "wbr"]
@@ -33,34 +36,114 @@
 
   RSS.prototype.render = function() {
     var self          = this
-      , entryTemplate = this.options.entryTemplate
 
     this.load(function(data) {
       self.entries = data.responseData.feed.entries
 
-      jQuery(self.entries).each(function() {
-        var entry     = this
-          , entryHTML = entryTemplate
+      var html = self.generateHTMLForEntries()
 
-        if(self.isRelevant(entry)) {
-          var evaluatedString = self.evaluateStringForEntry(entryTemplate, entry)
-          self.html.push(evaluatedString)
-        }
-      })
+      self.target.append(html.layout)
 
-      var html = self.html.join("\n")
+      if (html.entries.length !== 0) {
+        self.appendEntriesAndApplyEffects(html.layout, html.entries)
+      }
 
-      if(!!self.options.entryTemplate)
-        html = self.options.layoutTemplate.replace("{entries}", html)
-      else
-        html = (jQuery(html).length == 0) ? jQuery("<div>" + html + "</div>") : jQuery(html)
-
-      self.target.append(html)
-
-      if ($.isFunction(self.callback)) {
-        self.callback.call(self);
+      if (self.effectQueue.length > 0) {
+        self.executeEffectQueue(self.callback)
+      } else {
+        $.isFunction(self.callback) && self.callback.call(self);
       }
     })
+  }
+
+  RSS.prototype.appendEntriesAndApplyEffects = function(target, entries) {
+    var self = this
+
+    entries.forEach(function(entry) {
+      var $html = self.wrapContent(entry)
+
+      if(self.options.effect === 'show') {
+        target.append($html)
+      } else {
+        $html.css({ display: 'none' })
+        self.target.append($html)
+        self.applyEffect($html, self.options.effect)
+      }
+    })
+  }
+
+  RSS.prototype.generateHTMLForEntries = function() {
+    var self   = this
+      , result = {
+          entries: [],
+          layout:  null
+        }
+
+    jQuery(this.entries).each(function() {
+      var entry     = this
+
+      if(self.isRelevant(entry)) {
+        var evaluatedString = self.evaluateStringForEntry(self.options.entryTemplate, entry)
+        result.entries.push(evaluatedString)
+      }
+    })
+
+    if(!!this.options.entryTemplate) {
+      // we have an entryTemplate
+      result.layout = this.wrapContent(this.options.layoutTemplate.replace("{entries}", ''))
+    } else {
+      // no entryTemplate available
+      result.layout = this.wrapContent(result.entries.join("\n"))
+    }
+
+    return result
+  }
+
+  RSS.prototype.wrapContent = function(content) {
+    if($.trim(content).indexOf('<') !== 0) {
+      // the content has no html => create a surrounding div
+      return $("<div>" + content + "</div>")
+    } else {
+      // the content has html => don't touch it
+      return $(content)
+    }
+  }
+
+  RSS.prototype.applyEffect = function($element, effect, callback) {
+    var self = this
+
+    switch(effect) {
+      case 'slide':
+        $element.slideDown('slow', callback)
+        break
+      case 'slideFast':
+        $element.slideDown(callback)
+        break
+      case 'slideSynced':
+        self.effectQueue.push({ element: $element, effect: 'slide' })
+        break
+      case 'slideFastSynced':
+        self.effectQueue.push({ element: $element, effect: 'slideFast' })
+        break
+    }
+  }
+
+  RSS.prototype.executeEffectQueue = function(callback) {
+    var self = this
+
+    this.effectQueue.reverse()
+
+    var executeEffectQueueItem = function() {
+      var item = self.effectQueue.pop()
+
+      if(item) {
+        self.applyEffect(item.element, item.effect, executeEffectQueueItem)
+      } else {
+        callback && callback()
+      }
+    }
+
+    executeEffectQueueItem()
   }
 
   RSS.prototype.evaluateStringForEntry = function(string, entry) {
