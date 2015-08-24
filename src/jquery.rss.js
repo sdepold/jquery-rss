d3b366e16bcee93c64d9958b29d543f7e5a95839
Sascha Depold <sascha@depold.com>
2015-08-24 20:11:47 +0200

    [Changed] Use www.feedrapp.info

--- src/jquery.rss.js
+++ src/jquery.rss.js
@@ -10,7 +10,7 @@
 
     this.options = $.extend({
       ssl: false,
-      host: 'feedrapp.info',
+      host: 'www.feedrapp.info',
       limit: null,
       key: null,
       layoutTemplate: '<ul>{entries}</ul>',
