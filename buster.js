802cf6714e26f510f90f43f0a66320fb2bcceeae
Sascha Depold <sascha@depold.com>
2017-01-02 13:53:18 +0100

    Remove bower

--- buster.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var config = module.exports;
-config["integration"] = {"name":"integration","environment":"browser","rootPath":"/Users/sdepold/Projects/jquery-rss","libs":["lib/*.js","node_modules/moment/min/moment-with-locales.min.js"],"sources":["dist/jquery.rss.min.js"],"tests":["spec/jquery.rss.spec.js"]}
