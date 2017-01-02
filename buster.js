24896e331403aa2fe4a6d53e054207b4a89d91b5
Sascha Depold <sascha@depold.com>
2017-01-02 13:33:08 +0100

    Only build with node 4

---
+++ buster.js
@@ -0,0 +1,3 @@
+"use strict";
+var config = module.exports;
+config["integration"] = {"name":"integration","environment":"browser","rootPath":"/Users/sdepold/Projects/jquery-rss","libs":["lib/*.js","node_modules/moment/min/moment-with-locales.min.js"],"sources":["dist/jquery.rss.min.js"],"tests":["spec/jquery.rss.spec.js"]}
